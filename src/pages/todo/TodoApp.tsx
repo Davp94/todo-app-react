90c8b3c295f1edaff575ed5dc94277c797541708
david_blumbit <david.villca.pacheco@gmail.com>
2025-07-07 22:31:50 -0400

    create project structure

---
+++ src/pages/todo/TodoApp.tsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from 'react'
+
+import './App.css'
+
+function TodoApp() {
+
+  //FRAGMENT CONTENEDOR
+  return (
+    <>
+        <div></div>
+        <button></button>
+    </>
+  )
+}
+
+export default TodoApp
